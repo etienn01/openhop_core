5ea79035bed6a9db9f4a9cb4d47703e9cf2ded7b
Rightup <lloyd@rightup.co.uk>
2026-07-02 17:23:41 +0100

    refactor(MultipartAckHandler): improve ACK callback handling to support awaitable results

--- src/openhop_core/node/handlers/multipart.py
+++ src/openhop_core/node/handlers/multipart.py
@@ -28,7 +28,9 @@ class MultipartAckHandler(BaseHandler):
 
     def __init__(self, log_fn):
         self.log = log_fn
-        self._ack_received_callback: Optional[Callable[[int], Awaitable[None] | None]] = None
+        self._ack_received_callback: Optional[
+            Callable[[int], Awaitable[None] | None]
+        ] = None
 
     def set_ack_received_callback(
         self, callback: Optional[Callable[[int], Awaitable[None] | None]]
@@ -63,7 +65,6 @@ class MultipartAckHandler(BaseHandler):
         """Notify the dispatcher that an ACK was received."""
         if self._ack_received_callback:
             cb = self._ack_received_callback
-            if inspect.iscoroutinefunction(cb):
-                await cb(crc)
-            else:
-                cb(crc)
+            result = cb(crc)
+            if inspect.isawaitable(result):
+                await result
