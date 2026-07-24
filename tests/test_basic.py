e2ca8f9e40b325533fa1adc3a60e419266ecca80
Rightup <lloyd@rightup.co.uk>
2026-07-24 16:19:22 +0100

    bump version to 1.1.3.dev12 and update related tests

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -2,7 +2,7 @@ from openhop_core import CryptoUtils, LocalIdentity, MeshNode, Packet, __version
 
 
 def test_version():
-    assert __version__ == "1.1.3.dev11"
+    assert __version__ == "1.1.3.dev12"
 
 
 def test_import():
